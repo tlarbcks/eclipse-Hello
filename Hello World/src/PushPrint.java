b39d9dc24d312e7de8fe838c41c323ea541a81db
tlarbcks <34057674+tlarbcks@users.noreply.github.com>
2017-12-05 18:48:43 +0900

    pushprint.java modify

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -1,7 +1,7 @@
 
 public class PushPrint {
 	public static void main(String[] args) {
-		System.out.println("Add Java Class file push");			sss		
-System.out.println("afddsffds");
+		System.out.println("Add Java Class file push");					
+                System.out.println("afddsffds");
 	}
 }
